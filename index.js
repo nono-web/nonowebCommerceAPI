769feecc4f3fd8abb56258869c76845a98edc551
[REM-FR]Arnaud Didier <84334523+nono-web@users.noreply.github.com>
2022-07-08 14:36:33 +0200

    test

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ mongoose
   .then(() => console.log('MongoDB connected!'))
   .catch((err) => console.log(err));
 
-app.use(cors())  
+  app.use(cors({
+    origin: process.env.CLIENT_URL,
+  }));
+  
 app.use(express.json());
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
